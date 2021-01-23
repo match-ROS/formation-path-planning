d8a4f8d603e1305add1e4eabc76a2435b6bf8583
Henrik Lurz <henrik.lurz@web.de>
2021-01-23 18:50:20 +0100

    Added Doxygen comments to RobotInfo

--- fpp_ros/include/fpp_ros/data_classes/robot_info.h
+++ fpp_ros/include/fpp_ros/data_classes/robot_info.h
@@ -6,12 +6,22 @@
 
 namespace fpp_data_classes
 {
+    /**
+     * @brief Contains all information for each robot that is in the formation
+     * Data is read from the global_planner_config.yaml and loaded by move_base_flex
+     * 
+     */
     struct RobotInfo
     {
+        //! Name of the robot for identification
         std::string robot_name;
+        //! Namespace of the robot so it is possible to send topics/services/actions to the robot
         std::string robot_namespace;
+        //! Definition if this robot is the master of the formation
         bool fpp_master;
+        //! Offset from the master robot. This vector is seen from the masterrobot. -1/-1 means left and behind the master robot.
         Eigen::Vector2f offset;
+        //! Definition for the outline of this robot
         std::vector<Eigen::Vector2f> robot_outline;
     };
 }
